6858573e2a94d0c3bdee6c69ad5e97847e711fcb
MUDIT GAUR <91221391+muditgaur-1009@users.noreply.github.com>
2023-05-15 22:56:45 +0530

    problem Number 709

---
+++ 709. To Lower Case.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i=0;i<s.length();i++)
+          if (s[i]>='A'&& s[i]<='Z') 
+                s[i]+=32;
+                return s;
+    }
+};
+int main()
+{
+
+    return 0;
+}
