2474a0d252656eba4416319e3f01c896538e6532
MUDIT GAUR <91221391+muditgaur-1009@users.noreply.github.com>
2023-05-19 23:40:22 +0530

    Problem Number 283

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(int j=0;j<nums.size();j++)
+        {
+            
+            if(nums[j]!=0) nums[i++]=nums[j];
+        }
+        for(;i<nums.size();i++) nums[i]=0;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
