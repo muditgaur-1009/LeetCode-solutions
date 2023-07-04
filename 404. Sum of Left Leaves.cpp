12061c8f904e5a64db2bfd19f63ba809f46b4db8
MUDIT GAUR <91221391+muditgaur-1009@users.noreply.github.com>
2023-07-04 23:38:47 +0530

    Problem Number - 404

---
+++ 404. Sum of Left Leaves.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == NULL) 
+            return 0;
+        int sum = 0;
+        if (root->left != NULL && root->left->left == NULL && root->left->right == NULL)  //(1)
+            sum =sum+ root->left->val;
+           sum=sum+ sumOfLeftLeaves(root->left);
+        sum =sum+ sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
